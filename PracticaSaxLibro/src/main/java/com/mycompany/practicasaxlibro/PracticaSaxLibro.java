67a2ced9b44699d6a16718ce7dd051030544a3dd
Aman21 <112637273+P0ll021@users.noreply.github.com>
2023-10-17 09:42:54 +0200

    Add files via upload

---
+++ PracticaSaxLibro/src/main/java/com/mycompany/practicasaxlibro/PracticaSaxLibro.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicasaxlibro;
+
+import java.io.File;
+
+/**
+ *
+ * @author Aman
+ */
+public class PracticaSaxLibro {
+
+    public static void main(String[] args) {
+		File f=new File("Libros.xml");
+		AccesoXMLSAX a =new AccesoXMLSAX();
+		a.parsearXMLconLibrosSAXhandler(f);
+              //  a.parsearXMLconTitulosSAXhandler(f);
+	
+
+
+
+	}
+}
